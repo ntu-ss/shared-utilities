4455106cca0dcd70a2a1904a369e754f1eb67100
Petar Peychev <petarpeychev98@gmail.com>
2020-03-27 15:32:26 +0000

    Fixed bug by making message serialisable.

--- src/shared/utilities/Message.java
+++ src/shared/utilities/Message.java
@@ -1,6 +1,8 @@
 package shared.utilities;
 
+import java.io.Serializable;
+
 /**
  * Represents any message sent to or by the server.
  */
-public abstract class Message {}
+public abstract class Message implements Serializable {}
